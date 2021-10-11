04eefda32a88007570437c21bc0db9e8dfd38bee
Mike Unge <mikeunge@protonmail.com>
2021-10-11 17:59:13 +0200

    add: registration/login forms

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'timely'
+    "crispy_forms",
+    "crispy_tailwind",
+	'timely',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +53,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'app.urls'
+
+# Add crispy forms with tailwind
+CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
+CRISPY_TEMPLATE_PACK = "tailwind"
 
 TEMPLATES = [
     {
