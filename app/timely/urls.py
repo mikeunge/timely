71ec5da13ec200bcd5f062184ef7ab4d0d1a73ab
Mike Unge <mikeunge@protonmail.com>
2021-10-12 12:01:03 +0200

    add: reworked the TailwindCSS (now compiled with postcss), login form is now looking good but not working properly...

--- app/timely/urls.py
+++ app/timely/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('register', views.register_request, name='register'),
     path('login', views.login_request, name='login')
 ]
--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 NODE_ENV=production
 TAILWIND_MODE=build
-npx tailwindcss -o ./app/static/css/tailwind.css --jit --minify
+postcss app/timely/static/css/styles.css -o app/timely/static/css/tailwind.css
